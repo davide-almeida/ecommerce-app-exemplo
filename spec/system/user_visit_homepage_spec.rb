38e45e5d5c25a071e4886ed21ef2d981cc2af8bb
Davide Almeida <contato@davidealmeida.com.br>
2022-06-13 21:57:47 -0300

    feat: warehouse details (show)

--- spec/system/user_visit_homepage_spec.rb
+++ spec/system/user_visit_homepage_spec.rb
@@ -29,4 +29,43 @@ describe "Usuário visita tela inicial" do
     expect(page).to have_content 'Nenhum galpão encontrado'
 
   end
+
+  it "e vê detalhes de um galpão" do
+    # Arrange
+    json_data = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses').and_return(fake_response)
+    
+    json_data = File.read(Rails.root.join('spec/support/json/warehouse.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses/1').and_return(fake_response)
+
+    # Act
+    visit root_path
+    click_on 'Aeroporto TESTE'
+
+    # Assert
+    expect(page).to have_content 'Aeroporto TESTE'
+
+  end
+
+  it "e não é possível carregar o galpão" do
+    # Arrange
+    json_data = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double("faraday_response", status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses').and_return(fake_response)
+    
+    json_data = File.read(Rails.root.join('spec/support/json/warehouse.json'))
+    error_response = double("faraday_response", status: 500, body: "{}")
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/warehouses/1').and_return(error_response)
+
+    # Act
+    visit root_path
+    click_on 'Aeroporto TESTE'
+
+    # Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Não foi possível carregar o galpão'
+
+  end
 end
